e8cd17a55717fb135ecd814d6bcedf8211cf4bae
crudbits <crudbits@gmail.com>
2018-06-06 10:41:43 +0200

    updated settings

--- booksamples/arrays/src/main.rs
+++ booksamples/arrays/src/main.rs
@@ -1,18 +1,20 @@
 
-
-
 fn main() {
-  let xa:[i32; 10] = [1; 10];
+  let xa: [i32; 10] = [1; 10];
+
   let xv = vec![2; 10];
-  
+
   // let b = xv.iter();
   // println!("b = {:?}", b);
 
-  for (i,x) in xv.iter().enumerate() {
+  for (i, x) in xv.iter().enumerate() {
     if i == 2 {
-      break
+      break;
     }
     println!("x = {}", x);
+
+    let a: i32 = 1;
+    println!("a is {}", a);
   }
 
   for i in xv {
